e2214abdcc42341b69a1489fc87617c21a5d5aff
wjbrowneyed <theleesamuel@gmail.com>
2017-01-30 12:58:55 -0800

    start rails project and setup twitter api

---
+++ lib/twitter_api.rb
@@ -0,0 +1,12 @@
+class TwitterApi
+  def self.tweets
+    client.search('U+', options = {})
+  end
+
+  def self.client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+    end
+  end
+end
