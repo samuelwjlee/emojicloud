09445bbab768674c6ae149583feff0d34f97af5c
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-30 21:35:06 -0800

    Twitter stream

--- Twitter/config/initializers/twitter.rb
+++ Twitter/config/initializers/twitter.rb
@@ -6,13 +6,3 @@ $twitter = Twitter::REST::Client.new do |config|
   config.access_token = ENV['YOUR_ACCESS_TOKEN']
   config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
 end
-
-
-
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "FrYhXypqYZdMJ3AnHTrOovbHj"
-  config.consumer_secret     = "eUWDnrUHcauyVY6ihoZa59a9FkxLA02cRIAWb3pjROttv6F5SN"
-  config.access_token        = "1943009184-c5mEpzdJ3cXbIGaaDNmtCJPme7i2YqGs4thKvRg"
-  config.access_token_secret = "ftg9as8x7CA8H6uPTgeChNDtzSlbyQSJ6RGDVFj2EQZSz"
-end
--- Twitter/lib/twitter_api.rb
+++ Twitter/lib/twitter_api.rb
@@ -1,13 +1,32 @@
 class TwitterApi
 
-  def self.our_public_tweets
-    client.user_timeline('BBCNews', count: 1, exclude_replies: true)
+  # def self.our_public_tweets
+  #   client.user_timeline('BBCNews', count: 1, exclude_replies: true)
+  # end
+  #
+  # def self.dt_tweets
+  #   client.user_timeline('Donald Trump', exclude_replies: true)
+  # end
+  #
+  # def self.client
+  #   @client ||= Twitter::REST::Client.new do |config|
+  #     config.consumer_key = ENV['CONSUMER_KEY']
+  #     config.consumer_secret = ENV['CONSUMER_SECRET']
+  #   end
+  # end
+
+  def self.tweets
+    client.filter(locations: '-122.75,36.8,-121.75,37.8') do |tweet|
+      puts tweet.text if tweet.is_a?(Twitter::Tweet)
+    end
   end
 
   def self.client
-    @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
+    @client ||= Twitter::Streaming::Client.new do |config|
+      config.consumer_key = ENV["CONSUMER_KEY"]
+      config.consumer_secret = ENV["CONSUMER_SECRET"]
+      config.access_token = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
     end
   end
 
