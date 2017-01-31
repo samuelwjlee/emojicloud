682aa7c751f1352fb4bec2d24b41d624c6f1bbd0
wjbrowneyed <theleesamuel@gmail.com>
2017-01-30 21:05:22 -0800

    start twitter stream api setup

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,12 +1,16 @@
 class TwitterApi
   def self.tweets
-    client.search('san francisco', options = {})
+    client.sample.each do |tweet|
+      puts tweet.text if tweet.is_a?(Twitter::Tweet)
+    end
   end
 
   def self.client
-    @client ||= Twitter::REST::Client.new do |config|
+    @client ||= Twitter::Streaming::Client.new do |config|
       config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
     end
   end
 end
