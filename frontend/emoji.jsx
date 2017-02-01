083682e7fb6645cae69e0fd8d4d68098625c1430
wjbrowneyed <theleesamuel@gmail.com>
2017-01-31 18:13:26 -0800

    implement backend api to send twitter emoji info

---
+++ frontend/emoji.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Emoji extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default Emoji;
