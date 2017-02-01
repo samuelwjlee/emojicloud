3de504e5ab7d3f8c08fa7fe582795e4f67be2e75
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-31 21:42:36 -0800

    update twitter request

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -24,7 +24,7 @@ class TwitterApi
       count += 1
       object.text.split(" ").each do |word|
         if emoji2.join("").include?(word)
-          if word_cloud[word] 
+          if word_cloud[word]
             word_cloud[word] += 1
           else
             word_cloud[word] = 1
@@ -34,6 +34,41 @@ class TwitterApi
       return word_cloud if count == 300
     end
   end
+
+  def self.emoji2
+    emoji = EmojiData.all
+    emoji2 = emoji.slice(678, 77)
+    emoji2 << emoji[97] << emoji[111] << emoji[164] << emoji[299] << emoji[483] << emoji[505]
+    emoji = emoji.slice(700, 100)
+
+    puts emoji.length
+    emoji2.each_with_index do |emoji, i|
+      puts i
+      puts emoji
+    end
+    puts emoji2.length
+    heart = 0
+    word_cloud = {}
+    client.filter(track: emoji2.join(",")) do |object|
+      object.text.split(" ").each do |word|
+        # puts word
+        if emoji2.join("").include?(word)
+          if word_cloud[word]
+            word_cloud[word] += 1
+          else
+            word_cloud[word] = 1
+          end
+        end
+        # common = ["RT", "the", "a", "you", "I", "me", "to", "at", "and", "so", "for", "in", "de", "my", "que", "no", "this", "is", "of", "This"]
+        if word_cloud[word]  #&& !common.include?(word) && emoji2.join(",").include?(word)
+          puts "#{word}: #{word_cloud[word]}"
+        end
+      end
+
+      # puts object.text if object.is_a?(Twitter::Tweet)
+    end
+  end
+
 
   def self.client
     @client ||= Twitter::Streaming::Client.new do |config|
