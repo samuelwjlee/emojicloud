d4447fbdcc9227dafd63c27a811e69f5e32640d8
wjbrowneyed <theleesamuel@gmail.com>
2017-02-01 17:49:11 -0800

    configure routes for different regions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :emojis, only: [:index]
-
+    resources :asia_emojis, only: [:index]
+    resources :africa_emojis, only: [:index]
+    resources :europe_emojis, only: [:index]
+    resources :us_emojis, only: [:index]
   end
 end
