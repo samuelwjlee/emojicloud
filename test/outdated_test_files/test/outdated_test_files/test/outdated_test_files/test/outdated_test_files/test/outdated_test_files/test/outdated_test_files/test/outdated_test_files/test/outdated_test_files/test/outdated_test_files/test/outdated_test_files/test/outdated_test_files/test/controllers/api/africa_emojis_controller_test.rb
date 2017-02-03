afbc1150e377f030ef0c019f9f8ce9829c4acf57
Mark Noizumi <mark.noizumi@gmail.com>
2017-02-02 22:37:21 -0800

    api request changes

--- test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/controllers/api/africa_emojis_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::AfricaEmojisControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/outdated_test_files/test/controllers/api/asia_emojis_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::AsiaEmojisControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
