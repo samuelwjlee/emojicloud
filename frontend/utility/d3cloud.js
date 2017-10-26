1303a6b995a929d84e1c9553b16f9375c13f87cb
Samuel Lee <theleesamuel@gmail.com>
2017-10-25 23:22:38 -0700

    tweak d3cloud to take different emoji data from the backend

--- frontend/utility/d3cloud.js
+++ frontend/utility/d3cloud.js
@@ -20,8 +20,10 @@ export function fetchEmojis(place) {
   svg = d3.select("#cloud");
   node = svg.selectAll(".node");
   let apiAddress = '/api/' + place + '_emojis' + '/1'
-
   d3.json(apiAddress, function(data) {
+
+    console.log('getting data...', data);
+
     svg.selectAll("*").remove();
     while (nodes.length > 0) {nodes.pop();}
     emojis = getEmojis(data.emojis);
@@ -31,11 +33,17 @@ export function fetchEmojis(place) {
 
 function addEmoji() {
   let delay = 0;
+  // take element out of hash map
   let emoji = emojis.pop();
-  emoji.x = width/2;
-  emoji.y = height/2;
-  nodes.push(emoji);
-  start();
+
+  if (emoji !== 'top' && emoji !== 'total') {
+    emoji.x = width/2;
+    emoji.y = height/2;
+    nodes.push(emoji);
+
+    start();
+  }
+
   if (emojis.length > 0) {
     setTimeout(function () {
       addEmoji();
@@ -46,30 +54,25 @@ function addEmoji() {
 
 function getEmojis(emojis) {
   let totalVolume = 0;
-  let totalCount = 0;
   let minCount, maxCount;
 
-  Object.values(emojis).forEach((arr, idx) => {
-    let count = arr[0];
-    totalVolume += ((1 + Math.log(count)) * (1 + Math.log(count)));
-    totalCount += count;
-    if (idx === 0) {
-      minCount = count;
-      maxCount = count;
-    } else {
-      if (count < minCount) minCount = count;
-      if (count > maxCount) maxCount = count;
+  Object.keys(emojis).forEach((emoji) => {
+    let count = emojis[emoji];
+    if (emoji !== 'top' && emoji !== 'total') {
+      totalVolume += ((1 + Math.log(count)) * (1 + Math.log(count)));
+      count < minCount ? minCount = count : maxCount = count;
     }
-  });
+  })
 
   return Object.keys(emojis).map(function(emoji) {
-    return {
-      emojiData: emojis[emoji],
-      emojiFrequency: (emojis[emoji] * 100 / totalCount).toFixed(2),
-      imageUrl: emojione.unicodeToImage(emoji).match(/src="(.*)"/) ? emojione.unicodeToImage(emoji).match(/src="(.*)"/)[1] :  "https://cdn.jsdelivr.net/emojione/assets/png/1f611.png?v=2.2.7",
-      count: (1 + Math.log(emojis[emoji])) * getScalingFactor(totalVolume, minCount, maxCount)
-    };
-  });
+    if (emoji !== 'top' && emoji !== 'total') {
+      return {
+        emojiData: emoji,
+        imageUrl: emojione.unicodeToImage(emoji).match(/src="(.*)"/) ? emojione.unicodeToImage(emoji).match(/src="(.*)"/)[1] :  "https://cdn.jsdelivr.net/emojione/assets/png/1f611.png?v=2.2.7",
+        count: (1 + Math.log(emojis[emoji])) * getScalingFactor(totalVolume, minCount, maxCount)
+      }
+    }
+  })
 }
 
 function getScalingFactor(total, min, max) {
@@ -85,7 +88,6 @@ function start() {
         .attr("height", function (d) {return d.count;})
         .attr("class", function(d) {return "node";})
         .attr("emojiData", function(d) {return d.emojiData;})
-        .attr("emojiFrequency", function(d) {return d.emojiFrequency;})
     node.exit().remove();
     node.call(force.drag)
     force.start();
